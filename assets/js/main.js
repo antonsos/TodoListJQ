29a1be5e13d32069c185c60542c43f5ae2218656
Anton <mrservice.ip@gmail.com>
2018-06-15 21:47:34 +0700

    add logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
-$('.container__title-btn span').on('click', function() {
-  $('.container__title-btn span').toggleClass('fa-plus');
-  $('.container__title-btn span').toggleClass('fa-minus');
+$(function() {
+  $('.container__title-btn span').on('click', function() {
+    $(this).toggleClass('fa-plus');
+    $(this).toggleClass('fa-minus');
+    $('.container__input').fadeToggle('1000');
+  });
+
+  $('.container__list').on('click', '.container__item', function() {
+    $(this).toggleClass('completed');
+  });
+
+  $('.container__input').on('keypress', function(e) {
+    if(e.which === 13) {
+     var enterText = $(this).val();
+     $(this).val('');
+
+     $('.container__list').append(
+       '<li class="container__item">' + 
+       '<button class="container__btn fa fa-trash"></button>' + enterText + '</li>')
+    }
+  });
+
+  $('.container__list').on('click', '.container__btn', function() {
+    $(this).parent().fadeOut('1000', function() {
+      $(this).remove();
+    })
+  });
 });
